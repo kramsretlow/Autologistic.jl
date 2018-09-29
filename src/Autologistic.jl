5129eb17bf3a5b2e556ec6d5721fb6e589d6b3ed
mwolters <mark@mwolters.com>
2018-09-29 16:04:06 +0800

    small changes to imports exports

--- src/Autologistic.jl
+++ src/Autologistic.jl
@@ -22,19 +22,11 @@ include("abstractautologistic_type.jl")
 include("abstractunary_type.jl")
 include("abstractpairwise_type.jl")
 include("abstractcentering_type.jl")
-include("coding_type.jl")
 include("almodel_type.jl")
 include("fullunary_type.jl")
 include("linpredunary_type.jl")
 include("simplepairwise_type.jl")
 
-
-
-
-
-
 include("common.jl")
-
-greet() = print("Hello World!")
 
 end # module
